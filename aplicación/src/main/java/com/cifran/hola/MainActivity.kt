3ef379d363a626404eae90002e2a7f188e38b850
sdpandaloli3 <sdpandaloli3GitHub@gmail.com>
2026-06-06 19:40:20 -0400

    proyecto completo desde mi celular

---
+++ aplicación/src/main/java/com/cifran/hola/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cifran.hola
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
